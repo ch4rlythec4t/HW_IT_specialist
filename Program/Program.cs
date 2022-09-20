cb8befbfa0b44bb92317ba68b60302330300d88e
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-09-20 16:54:46 +0300

    program done

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static string[] SelectArray(string[] inputStringArray)
+{
+    int j = 0;
+    string[] newStringArray = new string[inputStringArray.Length];
+
+    for (int i = 0; i < inputStringArray.Length; i++)
+    {
+        if (inputStringArray[i].Length <= 3)
+        {
+            newStringArray[j] = inputStringArray[i];
+            j++;
+        }
+    }
+    newStringArray = newStringArray.Where(x => x != null).ToArray();
+
+    return newStringArray;
+}
+
+static void WriteArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write(@"[""" + array[i] + @""", ");
+        else if (i == array.Length - 1) Console.Write(@"""" + array[i] + @"""]");
+        else Console.Write(@"""" + array[i] + @""", ");
+    }
+}
+
+Console.WriteLine("Введите элементы строкового массива через запятую:");
+string? taskString = Console.ReadLine();
+string[] stringArray = taskString!.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+WriteArray(array: stringArray);
+Console.Write(" -> ");
+WriteArray(array: SelectArray(inputStringArray: stringArray));
